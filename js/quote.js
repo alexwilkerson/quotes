829578c9295854ec418a60c143c4e34c60d7051d
Alex Wilkerson <alex@uncorrected.com>
2016-07-14 01:09:58 -0500

    working :)

--- js/quote.js
+++ js/quote.js
@@ -3,33 +3,20 @@ var quotes;
 
 var quoteCard = document.getElementById("quote-card");
 
-    /*
-var randomQuote = function() {
-    console.log("random quote");
-    var parentofCard = quoteCard.parentNode;
-    quoteCard.className = "quote-card box-shadow deleting";
-    quoteCard = createNewCard();
-    document.getElementById("center").appendChild(quoteCard);
-    quoteCard.className = "quote-card box-shadow";
-    quoteCard = createNewCard();
-    quoteCard.onclick = randomQuote;
-    quoteCard.className = "quote-card box-shadow new";
-    quoteCard.parentNode.appendChild(quoteCard);
-    quoteCard.className = "quote-card box-shadow";
-}*/
-
 var randomQuote = function(event) {
     if (event.propertyName==="left"){
-        console.log("randomQuote called");
         var parentOfCard = quoteCard.parentNode;
         parentOfCard.removeChild(quoteCard);
         quoteCard = createNewCard();
         parentOfCard.appendChild(quoteCard);
-        quoteCard.className = "quote-card box-shadow";
+        window.setTimeout(function() {
+            quoteCard.className = "quote-card box-shadow";
+        }, 50); // used to enable transisition reset
     }
 }
 
 var moveLeft = function() {
+    quoteCard.addEventListener("transitionend", randomQuote, false);
     quoteCard.className = "quote-card box-shadow deleting";
 }
 
@@ -45,7 +32,6 @@ function createNewCard() {
     newCard.id = "quote-card";
     newCard.className = "quote-card box-shadow new";
     newCard.onclick = moveLeft;
-    newCard.addEventListener("transitionend", randomQuote, false);
 
     quote.innerText = "this is all a test";
 
@@ -60,11 +46,5 @@ function createNewCard() {
 
     return newCard;
 }
-// when figure is clicked
-    // move figure offscreen
-    // remove
-    // add figure with random quote, and random color
-    // move new firue on screen
 
 quoteCard.onclick = moveLeft;
-quoteCard.addEventListener("transitionend", randomQuote, false);
